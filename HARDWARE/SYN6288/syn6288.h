3953545bdbb54fad67b321a2ae6eb79f5e8904ef
cc <155149794+I-am-c@users.noreply.github.com>
2024-11-17 22:14:17 +0800

    Add files via upload

---
+++ HARDWARE/SYN6288/syn6288.h
@@ -0,0 +1,13 @@
+#ifndef __SYN6288_H
+#define __SYN6288_H
+
+#include "sys.h"
+#include "syn6288.h"
+#include "usart.h"
+#include "string.h"
+
+void SYN_FrameInfo( u8 *HZdata);
+void YS_SYN_Set(u8 *Info_data);
+
+#endif
+
